292c5da010f4720387036c2d59ad312c737a5eba
Aafaque Nazir <aafaquenazir@gmail.com>
2025-06-27 15:13:09 +0100

    placeholders and removed rounded 2xl

--- Pages/Contact.jsx
+++ Pages/Contact.jsx
@@ -5,7 +5,7 @@ function Contact() {
     <>
     <div id='contact' className='w-full  min-h-screen flex flex-col pb-28 bg-gradient-to-b to-teal-300 from-blue-300 '>
        
-        <div className=' space-x-6  w-full max-w-4xl mx-auto p-10 mt-24 bg-white rounded-3xl shadow-xl'>
+        <div className=' space-x-6  w-full max-w-4xl mx-auto p-10 mt-24 bg-white  shadow-xl'>
             <form className='space-y-6'
              action="https://formsubmit.co/aafaquebuisness@gmail.com" 
             method="POST" 
@@ -14,15 +14,15 @@ function Contact() {
             <h1 className='text-5xl font-bold text-center my-6'>Contact Us</h1>
                 <div>
                     <label className='block text-gray-700' htmlFor='name'>Name</label>
-                    <input className='w-full p-3 shadow-lg border border-gray-500 rounded-lg' type='text' id='name' name='name' required />
+                    <input className='w-full p-3 shadow-md border border-gray-500 rounded-lg' type='text' id='name' name='name' required placeholder='Aafaque Nazir'  />
                 </div>
                 <div>
                     <label className='block text-gray-700' htmlFor='email'>Email</label>
-                    <input className='w-full p-3 shadow-lg border border-gray-500 rounded-lg' type='email' id='email' name='email' required />
+                    <input className='w-full p-3 shadow-md border border-gray-500 rounded-lg' type='email' id='email' name='email' required placeholder='example@gmail.com' />
                 </div>
                 <div>
                     <label className='block text-gray-700' htmlFor='message'>Message</label>
-                    <textarea className='w-full p-3 shadow-lg border border-gray-500 rounded-lg' id='message' name='message' rows='5' required></textarea>
+                    <textarea className='w-full p-3 shadow-md border border-gray-500 rounded-lg' id='message' name='message' rows='5' required placeholder='Type your message here'></textarea>
                 </div>
                 <button className='w-full bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700 transition duration-200'>Send Message</button>
             </form>
--- Pages/Destination.jsx
+++ Pages/Destination.jsx
@@ -24,11 +24,11 @@ function Destination() {
   return (
     <>
      <section id='destination' className="bg-gradient-to-b from-teal-300 to-blue-300 py-10">
-      <h2 className="text-4xl font-bold text-white text-center mb-4">DESTINATIONS</h2>
-      <p className="text-white text-center mb-8">Explore the world’s most stunning places</p>
+      <h2 className="text-4xl font-bold text-black text-center mb-4">DESTINATIONS</h2>
+      <p className="text-black text-center text-xl mb-8">Explore the world’s most stunning places</p>
        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 px-6">
         {destinations.map((dest,index) => (
-            <div key={index} className="bg-white rounded-lg shadow-2xl overflow-hidden">
+            <div key={index} className="bg-white rounded-lg shadow-xl overflow-hidden">
                 <div className='bg-white drop-shadow-lg text-center py-2 font-semibold'>{dest.name}</div>
                 <img src={dest.image} className='w-full h-full object-cover' alt="" />
                 </div>
